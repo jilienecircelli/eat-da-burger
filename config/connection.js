19ec4e13f8b50a87ee9c7967687a303457990026
Jiliene Circelli <jilienecircelli@gmail.com>
2020-01-30 12:16:45 -0800

    updated connection for Jaws again..

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,13 @@
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-        port: 3306,
-        user: "dks4b2wzta4dhqhv",
-        password: "ph4ugq2r8ac5pnrs",
-        database: "fgmtkq6ztf198yem"
-    })
-}
+
+var connection = mysql.createConnection({
+    host: "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+    port: 3306,
+    username: "dks4b2wzta4dhqhv",
+    password: "ph4ugq2r8ac5pnrs",
+    database: "fgmtkq6ztf198yem"
+})
 
 connection.connect(function(err) {
     if (err) {
