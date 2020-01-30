ac09a2c07cfd7f8184146e93c0944e949bd16aaa
Jiliene Circelli <jilienecircelli@gmail.com>
2020-01-30 12:27:07 -0800

    fixed issues, maybe

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,22 @@
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "mysqlmysql",
+        database: "burgers_db"
+    })
 
-var connection = mysql.createConnection({
-    host: "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
-    port: 3306,
-    username: "dks4b2wzta4dhqhv",
-    password: "ph4ugq2r8ac5pnrs",
-    database: "fgmtkq6ztf198yem"
-})
-
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
+    connection.connect(function(err) {
+        if (err) {
+            console.error("error connecting: " + err.stack);
+            return;
+        }
+        console.log("connected as id " + connection.threadId);
+    });
+}
 module.exports = connection;
